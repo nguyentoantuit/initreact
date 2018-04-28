60a2572eb0fd527797ed48d6f16d2f2fcbad4a6c
Toan <nguyentoanuit@gmail.com>
2018-04-28 18:16:20 +0700

    fix wrong webpack config

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -1,29 +1,29 @@
 var webpack = require('webpack');
 var path = require('path');
 
+//var parentDir = path.join(__dirname, '../');
+
 module.exports = {
     entry: [
         path.join(__dirname, '../index.js')
     ],
     module: {
-        loaders: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.less$/,
-                loaders: ["style-loader", "css-loader", "less-loader"]
-            }
+        loaders: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }, {
+            test: /\.less$/,
+            loaders: ["style-loader", "css-loader", "less-loader"]
+        }
         ]
     },
     output: {
-        path: __dirname + '/dist',
+        path: path.join(__dirname, '../dist'),
         filename: 'bundle.js'
     },
     devServer: {
-        contentBase: __dirname + '/dist',
+        contentBase: path.join(__dirname, '../'),
         historyApiFallback: true
     }
 }
